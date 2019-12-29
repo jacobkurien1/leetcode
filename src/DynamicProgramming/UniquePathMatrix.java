e75e65c52bd2f09dbcdfc3759f10f20ddd34b17b
Jacob Kurien <jacobofficial@hotmail.com>
2019-12-28 16:25:40 -0800

    unique path in a matrix

---
+++ src/DynamicProgramming/UniquePathMatrix.java
@@ -0,0 +1,42 @@
+package DynamicProgramming;
+
+/*
+https://leetcode.com/problems/unique-paths/
+A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
+
+The robot can only move either down or right at any point in time.
+The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+
+How many possible unique paths are there?
+
+Note: m and n will be at most 100.
+
+Example 1:
+
+Input: m = 3, n = 2
+Output: 3
+Explanation:
+From the top-left corner, there are a total of 3 ways to reach the bottom-right corner:
+1. Right -> Right -> Down
+2. Right -> Down -> Right
+3. Down -> Right -> Right
+Example 2:
+
+Input: m = 7, n = 3
+Output: 28
+ */
+public class UniquePathMatrix {
+    public int uniquePaths(int m, int n) {
+        if(m<=0 || n<=0){
+            return 0;
+        }
+        int[] curr = new int[m];
+        curr[0] = 1;
+        for(int i = 0; i<n; i++){
+            for(int j = 0; j<m; j++){
+                curr[j] += ((j-1>=0)?curr[j-1]:0);
+            }
+        }
+        return curr[m-1];
+    }
+}
