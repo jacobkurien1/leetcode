65e2b8622d6631f286f114374a6a5f1fd9c0163e
Jacob Kurien <jacobofficial@hotmail.com>
2020-01-05 01:08:01 -0800

    trees simple problems

---
+++ src/Trees/InvertTree.java
@@ -0,0 +1,43 @@
+package Trees;
+
+/*
+https://leetcode.com/problems/invert-binary-tree/
+Invert a binary tree.
+
+Example:
+
+Input:
+
+     4
+   /   \
+  2     7
+ / \   / \
+1   3 6   9
+Output:
+
+     4
+   /   \
+  7     2
+ / \   / \
+9   6 3   1
+ */
+public class InvertTree {
+    public TreeNode invertTree(TreeNode root) {
+        if(root == null){
+            return null;
+        }
+        invertTree(root.left);
+        invertTree(root.right);
+        TreeNode left = root.left;
+        root.left = root.right;
+        root.right = left;
+        return root;
+    }
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+}
---
+++ src/Trees/TreeDepth.java
@@ -0,0 +1,37 @@
+package Trees;
+
+/*
+https://leetcode.com/problems/maximum-depth-of-binary-tree/
+Given a binary tree, find its maximum depth.
+
+The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+Note: A leaf is a node with no children.
+
+Example:
+
+Given binary tree [3,9,20,null,null,15,7],
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its depth = 3.
+ */
+public class TreeDepth {
+    public int maxDepth(TreeNode root) {
+        if(root == null){
+            return 0;
+        }
+        int leftDepth = maxDepth(root.left);
+        int rightDepth = maxDepth(root.right);
+        return Math.max(leftDepth, rightDepth) + 1;
+    }
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+}
